3970d371773dabf3b20889966e87851685dc12c5
Nikita.Lysiuk <nikita.lysiuk@skni.umcs.pl>
2024-10-17 10:15:25 +0200

    table in exel

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "windows.h"
+
+void playSound(int sound)  {
+    Beep(sound, 1000);
+}
 
 int main() {
     std::cout << "Egypt adventure" << std::endl;
+    playSound(523);
     return 0;
 }
